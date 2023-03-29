3b1c388ed548dea39ec44952224a323bc1c0ccba
MateuszPisula01 <126687627+MateuszPisula01@users.noreply.github.com>
2023-03-29 12:35:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+
+    String znak="*"; 
+   
+     for (int i = 1; i <= 4; i++) 
+
+    {
+for (int y=1;y<=4;y++)
+  {
+
+    System.out.print(znak);
+  }
+
+    
+      System.out.println();
+ }
+}}
